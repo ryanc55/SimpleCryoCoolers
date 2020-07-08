8aadbac1bed77fc3b04821577068b0ad6c841111
ryanc55 <54254625+ryanc55@users.noreply.github.com>
2020-07-07 17:52:46 -0700

    Alpha 0.1

--- Source/cryocooler.cs
+++ Source/cryocooler.cs
@@ -10,91 +10,96 @@ namespace CryoCooler
     {
         static double lowestTankTemperature;
         static ModuleFuelTanks tankModule;
+        static int noCatchup = 0;
         static FuelTankList tankList = new FuelTankList();
-        static bool hasCryoFuels = false;
         readonly double maxCoolerPower = 5;  //kW
         readonly double maxEfficiency = 10;
             
         [KSPField(isPersistant = true, guiActive = true, guiName = "Cooler Enabled: ")]
-        public bool powerState = true;
+        public bool isPoweredOn = true;
         [KSPField(isPersistant = false, guiActive = true, guiName = "Cooler Efficiency: ", guiFormat = "F3")]
         public double coolerEfficiency = 1;
         [KSPField(isPersistant = false, guiActive = true, guiName = "Cooler Xfer: ", guiFormat ="F3", guiUnits ="kW")]
         public double heatFlux = 0;
-
-        public void Start()
+        [KSPField(isPersistant = true, guiActive = false)]
+        public double lastTankTemperature = 0;
+        public override void OnStart(StartState state)
         {
-            Debug.Log("[SCC] Starting CryoCooler " + Time.realtimeSinceStartup);
- //           electricResourceIndex = this.resHandler.inputResources.FindIndex(x => x.name == "ElectricCharge");
-            tankModule = base.part.parent.FindModuleImplementing<RealFuels.Tanks.ModuleFuelTanks>();
-            if (tankModule != null)
+            base.OnStart(state);
+            if (part.parent != null)
             {
-                tankList = GetInstanceField(tankModule, "tankList") as FuelTankList;
-                if (tankList != null)
+                Debug.Log("[SCC] Starting CryoCooler " + Time.realtimeSinceStartup);
+                tankModule = base.part.parent.FindModuleImplementing<RealFuels.Tanks.ModuleFuelTanks>();
+                if (tankModule != null)
                 {
-                    hasCryoFuels = CalculateLowestTankTemperature();
-                    powerState = true;
-                }
-            }
-            else
-            {
-                powerState = false;
-                Debug.Log("[SCC] Error: Not Attached to RF tank. ");
-            }
-
-           // base.part.SetHighlightColor(Color.yellow);
-        }
-
-        public void FixedUpdate()
-        {
-            //base.FixedUpdate();
-            if (tankModule != null)
-            {
-                if (hasCryoFuels)
-                {
-                    if (part.parent.temperature > lowestTankTemperature - .1 && part.temperature < part.maxTemp * .9 && powerState)
+                    tankList = GetInstanceField(tankModule, "tankList") as FuelTankList;
+                    if (tankList != null)
                     {
+                        CalculateLowestTankTemperature();
+                        if (isPoweredOn && lastTankTemperature > 0)
+                        {
+                            noCatchup = 10;
 
-                        if (part.temperature > part.parent.temperature) //don't work as a generator
-                        { 
-                            double carnotEfficiency = part.parent.temperature / (part.temperature - part.parent.temperature);
-                            coolerEfficiency = Math.Min(maxEfficiency,.2 * carnotEfficiency); // Very good for a small stirling
-                        } else {
-                            coolerEfficiency = maxEfficiency; 
+                            //part.parent.skinTemperature = lowestTankTemperature;
                         }
-
-                        double powerRequested = Math.Max(0,Math.Min(maxCoolerPower, maxCoolerPower * (1 + (part.parent.temperature - lowestTankTemperature) * 10 )));  //throttle to 0 at -.1 target
-                        double energyUsed = part.RequestResource("ElectricCharge", TimeWarp.fixedDeltaTime * powerRequested, ResourceFlowMode.ALL_VESSEL);
-                   
-                        heatFlux = energyUsed * coolerEfficiency / TimeWarp.fixedDeltaTime;
-                        part.parent.AddThermalFlux(-heatFlux);
-                        part.AddThermalFlux(energyUsed / TimeWarp.fixedDeltaTime + heatFlux);
-                        //part.parent.temperature = lowestTankTemperature - .1;
-                        //part.parent.skinTemperature = lowestTankTemperature;
-                        
                     }
                 }
+                else
+                {
+                    isPoweredOn = false;
+                    Debug.Log("[SCC] Error: Not Attached to RF tank. ");
+                }
+            }
+
+            // base.part.SetHighlightColor(Color.yellow);
+        }
+        
+
+        public void FixedUpdate()
+        { 
+            if (tankModule != null && HighLogic.LoadedSceneIsFlight && part.parent != null)
+            {
+                if (part.parent.temperature > lowestTankTemperature - .1 && part.temperature < part.maxTemp * .9 && isPoweredOn)
+                {
+
+                    if (part.temperature > part.parent.temperature) //don't work as a generator
+                    { 
+                        double carnotEfficiency = part.parent.temperature / (part.temperature - part.parent.temperature);
+                        coolerEfficiency = Math.Min(maxEfficiency,.2 * carnotEfficiency); // Very good for a small stirling
+                    } else {
+                        coolerEfficiency = maxEfficiency; 
+                    }
+                    if (noCatchup > 0) {
+                        part.parent.temperature = lastTankTemperature;
+                        noCatchup--;
+                    }
+                    double powerRequested = Math.Max(0,Math.Min(maxCoolerPower, maxCoolerPower * (1 + (part.parent.temperature - lowestTankTemperature) * 10 )));  //throttle to 0 at -.1 target
+                    double energyUsed = part.RequestResource("ElectricCharge", TimeWarp.fixedDeltaTime * powerRequested, ResourceFlowMode.ALL_VESSEL);
+                   
+                    heatFlux = energyUsed * coolerEfficiency / TimeWarp.fixedDeltaTime;
+                    part.parent.AddThermalFlux(-heatFlux);
+                    part.AddThermalFlux(energyUsed / TimeWarp.fixedDeltaTime + heatFlux);
+                    lastTankTemperature = part.parent.temperature;
+
+                }
             }
         }
-        private bool CalculateLowestTankTemperature()
+        private void CalculateLowestTankTemperature()
         {
-            bool result = false;
-            lowestTankTemperature = 300;
+            lowestTankTemperature = 500;
             for (int i = tankList.Count - 1; i >= 0; --i)
             {
                 RealFuels.Tanks.FuelTank tank = tankList[i];
                 if (tank.maxAmount > 0d && (tank.vsp > 0.0 || tank.loss_rate > 0d))
                 {
                     lowestTankTemperature = Math.Min(lowestTankTemperature, tank.temperature);
-                    result = true;
                 }
             }
-            return result;
         }
         [KSPEvent(guiActive = true, guiName = "Toggle Cooling")]
         public void CoolingToggle()
         {
-            powerState = !powerState;
+            isPoweredOn = !isPoweredOn;
         }
 
         internal static object GetInstanceField(object instance, string fieldName)
